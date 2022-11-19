6ae16ca812ba4e576b7324bd985f7a32199e63ce
SadPenguin24 <danhendrix.pro@gmail.com>
2022-11-20 02:50:41 +0800

    refresh finalized

--- apps/frontend/src/app/redux/api/apiSlice.js
+++ apps/frontend/src/app/redux/api/apiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { setCredentials, logOut } from '../slice/authSlice';
-import { getCookie } from 'cookies-next';
+import { getCookie, setCookie } from 'cookies-next';
 
 const port =
   process.env.NODE_ENV === 'development'
@@ -22,15 +22,30 @@ const baseQuery = fetchBaseQuery({
 const baseQueryWithReauth = async (args, api, extraOptions) => {
   let result = await baseQuery(args, api, extraOptions);
 
-  if (result?.error?.originalStatus === 403) {
+  //403
+  if (result?.error?.originalStatus === 403 || 401) {
     console.log('sending refresh token');
     // send refresh token to get new access token
-    const refreshResult = await baseQuery('/refresh', api, extraOptions);
-    console.log(refreshResult);
-    if (refreshResult?.data) {
-      const user = api.getState().auth.user;
+    const refresh_token = getCookie('refresh_token')
+    const refreshResult = await baseQuery({
+      url: '/auth/refresh',
+      method: 'POST',
+      body: {refresh_token}
+    }, api, extraOptions);
+    const refreshData = refreshResult.data;
+
+    if (refreshData) {
       // store the new token
-      api.dispatch(setCredentials({ ...refreshResult.data, user }));
+      const n_access_token = refreshData.access_token;
+      const n_refresh_token = refreshData.refresh_token;
+      setCookie('access_token', n_access_token, {
+        sameSite: 'strict',
+      });
+
+      setCookie('refresh_token', n_refresh_token, {
+        sameSite: 'strict',
+      });
+
       // retry the original query with new access token
       result = await baseQuery(args, api, extraOptions);
     } else {
