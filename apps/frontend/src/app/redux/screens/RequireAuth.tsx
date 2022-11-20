2ee4e9f5ad585cb2d9fe46b39c2cce446cfd76e8
SadPenguin24 <danhendrix.pro@gmail.com>
2022-11-20 15:05:49 +0800

    redirect function

--- apps/frontend/src/app/redux/screens/RequireAuth.tsx
+++ apps/frontend/src/app/redux/screens/RequireAuth.tsx
@@ -1,22 +1,37 @@
-import { useLocation, Navigate, Outlet } from 'react-router-dom';
+import { useLocation, Navigate, Outlet, useNavigate } from 'react-router-dom';
 import { getCookie } from 'cookies-next';
-//import { useAppSelector } from '../store';
-//import { selectCurrentUser } from '../slice/authSlice';
+import { useAppSelector } from '../store';
+import { selectCurrentUser } from '../slice/authSlice';
+import { useState } from 'react';
 // import NavBar from '../../components/navbar/NavBar';
 // import Sidebar from '../../components/sidebar/Sidebar';
 
 function RequireAuth() {
   const token = getCookie('access_token');
-  //rehydrate if there is a token
-  //const user = useAppSelector(selectCurrentUser);
+
+  const user = JSON.parse(localStorage.getItem('userInfo')!);
   const location = useLocation();
-  //darkMode = false
-  // darkMode ? theme="darkmode" : theme="lightMode"
+  const navigate = useNavigate();
+  let userValidator = true;
+
+  const userRole = user?.role[0];
+  userValidator = location.pathname.includes(userRole);
+
+  console.log(userValidator);
+
   const content = token ? (
-    <Outlet />
+    userValidator ? (
+      <>
+        <Outlet />
+      </>
+    ) : (
+      <>
+        {navigate(`/${userRole}/home`)} <Outlet />
+      </>
+    )
   ) : (
     <>
-      <Navigate to="/" state={{ from: location }} replace />
+      <Navigate to="/" state={{ from: location }} replace />;
       <Outlet />
     </>
   );
