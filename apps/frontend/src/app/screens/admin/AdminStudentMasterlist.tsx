3b78daba81870d3f110934d88cca147486657b86
cdvsalanga <cdvsalanga@gmail.com>
2022-11-22 14:35:09 +0800

    Show faculties in faculty masterlist

--- apps/frontend/src/app/screens/admin/AdminStudentMasterlist.tsx
+++ apps/frontend/src/app/screens/admin/AdminStudentMasterlist.tsx
@@ -45,59 +45,48 @@ function AdminStudentMasterlist() {
   } else if (isSuccess) {
     console.log('THIS IS PROFILE: ', students);
     content = (
-      <>
-        <div className="d-flex justify-content-end mb-3">
-          <div className="w-50">
-            <FormControl
-              style={{ backgroundColor: '#ffe4a0', border: '#eaaa79 solid' }}
-              placeholder="Enter Student Name"
-            ></FormControl>
-          </div>
-        </div>
-
-        <Table bordered className="tableColor" responsive="lg">
-          <thead style={{ backgroundColor: '#2a6fd6' }}>
-            <tr className="text-center">
-              <th className="textWhite">Actions</th>
-              <th className="textWhite">Student No.</th>
-              <th className="textWhite">Last Name</th>
-              <th className="textWhite">First Name</th>
-              <th className="textWhite">Middle Name</th>
-              <th className="textWhite">Grade Level</th>
-              <th className="textWhite">Age</th>
-              <th className="textWhite">Birthdate</th>
-              <th className="textWhite">Birthplace</th>
-              <th className="textWhite">Contact #</th>
-            </tr>
-          </thead>
-          <tbody>
-            {students ? (
-              students.map((student: any) => (
-                <tr key={student._id}>
-                  <td>
-                    <LinkContainer to="/admin/student">
-                      <Button>View Student</Button>
-                    </LinkContainer>
-                  </td>
-                  <td>{student.student._id}</td>
-                  <td>{student.last_name}</td>
-                  <td>{student.first_name}</td>
-                  <td>{student.middle_name}</td>
-                  <td>11</td>
-                  <td>17</td>
-                  <td>December 25, 2015</td>
-                  <td>Pangasinan</td>
-                  <td>09999999999</td>
-                </tr>
-              ))
-            ) : (
-              <tr>
-                <td colSpan={10}>No Students</td>
+      <Table bordered className="tableColor" responsive="lg">
+        <thead style={{ backgroundColor: '#2a6fd6' }}>
+          <tr className="text-center">
+            <th className="textWhite">Actions</th>
+            <th className="textWhite">Student No.</th>
+            <th className="textWhite">Last Name</th>
+            <th className="textWhite">First Name</th>
+            <th className="textWhite">Middle Name</th>
+            <th className="textWhite">Grade Level</th>
+            <th className="textWhite">Age</th>
+            <th className="textWhite">Birthdate</th>
+            <th className="textWhite">Birthplace</th>
+            <th className="textWhite">Contact #</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students ? (
+            students.map((student: any) => (
+              <tr key={student._id}>
+                <td>
+                  <LinkContainer to="/admin/student">
+                    <Button>View Student</Button>
+                  </LinkContainer>
+                </td>
+                <td>{student._id}</td>
+                <td>{student.last_name}</td>
+                <td>{student.first_name}</td>
+                <td>{student.middle_name}</td>
+                <td>11</td>
+                <td>17</td>
+                <td>December 25, 2015</td>
+                <td>Pangasinan</td>
+                <td>09999999999</td>
               </tr>
-            )}
-          </tbody>
-        </Table>
-      </>
+            ))
+          ) : (
+            <tr>
+              <td colSpan={10}>No Students</td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
     );
   } else if (isError) {
     content = <p>{JSON.stringify(error)}</p>;
@@ -106,7 +95,17 @@ function AdminStudentMasterlist() {
     <div className="mb-5">
       <style>{'body { background-color: #dcf7b0; }'}</style>
       <Header page="Student Masterlist" redirect="/admin/home" />
-      <Container>{content}</Container>
+      <Container>
+        <div className="d-flex justify-content-end mb-3">
+          <div className="w-50">
+            <FormControl
+              style={{ backgroundColor: '#ffe4a0', border: '#eaaa79 solid' }}
+              placeholder="Enter Student Name"
+            ></FormControl>
+          </div>
+        </div>
+        {content}
+      </Container>
     </div>
   );
 }
