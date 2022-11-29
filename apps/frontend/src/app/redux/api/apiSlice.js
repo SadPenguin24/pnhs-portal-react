d2a3c82444e1f68b735cfb2897690196dd5af3d8
cdvsalanga <cdvsalanga@gmail.com>
2022-11-30 03:15:00 +0800

    Fix

--- apps/frontend/src/app/redux/api/apiSlice.js
+++ apps/frontend/src/app/redux/api/apiSlice.js
@@ -61,6 +61,6 @@ const baseQueryWithReauth = async (args, api, extraOptions) => {
 
 export const apiSlice = createApi({
   baseQuery: baseQueryWithReauth,
-  tagTypes: ['Enrollee', 'Subject', 'Section', 'Schedule'],
+  tagTypes: ['Enrollee', 'Subject', 'Section', 'Schedule', 'User'],
   endpoints: (builder) => ({}),
 });
