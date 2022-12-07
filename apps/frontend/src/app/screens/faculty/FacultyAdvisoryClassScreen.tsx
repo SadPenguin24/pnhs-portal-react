88eafb34eafd5bf7fa17d00a8ad3b75e08b2b422
cdvsalanga <cdvsalanga@gmail.com>
2022-12-07 17:21:42 +0800

    Filter function in advisory class screen

--- apps/frontend/src/app/screens/faculty/FacultyAdvisoryClassScreen.tsx
+++ apps/frontend/src/app/screens/faculty/FacultyAdvisoryClassScreen.tsx
@@ -3,6 +3,7 @@ import {
   Button,
   Col,
   Container,
+  Form,
   FormGroup,
   FormLabel,
   FormSelect,
@@ -10,13 +11,18 @@ import {
   Spinner,
   Table,
 } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
 import Header from '../../components/header/Header';
 import '../../components/tables/tables.scss';
 import { useGetParsedSectionsQuery } from '../../redux/api/sectionApi';
 
 function FacultyAdvisoryClassScreen() {
   const [currentUser] = useState(JSON.parse(localStorage.getItem('userInfo')!));
-  console.log(currentUser);
+  const [advisorySection]: any = useState([]);
+  let [filteredSection, setFilteredSection]: any = useState([]);
+  const [isFiltering, setIsFiltering] = useState(false);
+
+  const { register, handleSubmit } = useForm();
 
   const {
     data: sections,
@@ -26,7 +32,52 @@ function FacultyAdvisoryClassScreen() {
     error,
   } = useGetParsedSectionsQuery({});
 
-  let content;
+  let content, facultySections;
+
+  const getAdvisoryClass = () => {
+    console.log('Get Advisory Class');
+    currentUser.faculty.advisory_section_ids.map((section: any) => {
+      facultySections = sections.find(
+        (advisorySection: any) => advisorySection._id === section
+      );
+      if (!advisorySection.includes(facultySections)) {
+        advisorySection.push(facultySections);
+      }
+    });
+  };
+
+  const filterSection = ({ gradeLvl, term, selectSection }: any) => {
+    if (gradeLvl === 'All' && term === 'All' && selectSection === 'All') {
+      getAdvisoryClass();
+      filteredSection = [];
+      setIsFiltering(false);
+    } else {
+      let filter = advisorySection;
+      console.log('Filter Sections');
+      if (gradeLvl !== 'All') {
+        filter = filter.filter(
+          (section: any) => section.grade_level === +gradeLvl
+        );
+
+        console.log(+gradeLvl, filteredSection);
+      }
+      if (term !== 'All') {
+        filter = filter.filter((section: any) => section.term === +term);
+
+        console.log(+term, filteredSection);
+      }
+      if (selectSection !== 'All') {
+        filter = filter.filter(
+          (section: any) => section.section_name === selectSection
+        );
+
+        console.log(selectSection, filteredSection);
+      }
+      setFilteredSection(filter);
+      setIsFiltering(true);
+    }
+    console.log(gradeLvl, term, selectSection);
+  };
 
   if (isLoading) {
     content = (
@@ -37,67 +88,117 @@ function FacultyAdvisoryClassScreen() {
       </div>
     );
   } else if (isSuccess) {
-    console.log(sections);
+    getAdvisoryClass();
+    console.log(advisorySection);
     content = (
       <>
-        <Row>
-          <FormGroup as={Col} className="d-lg-flex">
-            <FormLabel className="me-2 my-auto">Grade</FormLabel>
-            <FormSelect style={{ height: '37.6px' }}>
-              <option>All</option>
-              <option>11</option>
-              <option>12</option>
-            </FormSelect>
-          </FormGroup>
-          <FormGroup as={Col} className="d-lg-flex">
-            <FormLabel className="me-2 my-auto">Term</FormLabel>
-            <FormSelect style={{ height: '37.6px' }}>
-              <option>All</option>
-              <option>First semester</option>
-              <option>Second semester</option>
-            </FormSelect>
-          </FormGroup>
-          <FormGroup as={Col} className="d-lg-flex">
-            <FormLabel className="me-2 my-auto">Section/Strand</FormLabel>
-            <FormSelect style={{ height: '37.6px' }}>
-              <option>All</option>
-              <option>Section-1-ABM</option>
-              <option>Section-1-STEM</option>
-            </FormSelect>
-          </FormGroup>
-          <Col className="my-auto">
-            <Button variant="secondary">Load</Button>
-          </Col>
-        </Row>
+        {currentUser.faculty.advisory_section_ids.length > 0 && (
+          <Form onSubmit={handleSubmit(filterSection)}>
+            <Row>
+              <Form.Group as={Col} className="d-lg-flex">
+                <Form.Label className="me-2 my-auto">Grade</Form.Label>
+                <Form.Select
+                  {...register('gradeLvl')}
+                  style={{ height: '37.6px' }}
+                >
+                  <option>All</option>
+                  {advisorySection.find(
+                    ({ grade_level }: any) => grade_level === 11
+                  ) && <option>{11}</option>}
+                  {advisorySection.find(
+                    ({ grade_level }: any) => grade_level === 12
+                  ) && <option>{12}</option>}
+                </Form.Select>
+              </Form.Group>
+              <Form.Group as={Col} className="d-lg-flex">
+                <Form.Label className="me-2 my-auto">Term</Form.Label>
+                <Form.Select {...register('term')} style={{ height: '37.6px' }}>
+                  <option>All</option>
+                  {advisorySection.find(({ term }: any) => term === 1) && (
+                    <option>{1}</option>
+                  )}
+                  {advisorySection.find(({ term }: any) => term === 2) && (
+                    <option>{2}</option>
+                  )}
+                </Form.Select>
+              </Form.Group>
+              <Form.Group as={Col} className="d-lg-flex">
+                <Form.Label className="me-2 my-auto">Section</Form.Label>
+                <Form.Select
+                  {...register('selectSection')}
+                  style={{ height: '37.6px' }}
+                >
+                  <option>All</option>
+                  {advisorySection.map((section: any) => (
+                    <option key={section._id} value={section.section_name}>
+                      {section.section_name}
+                    </option>
+                  ))}
+                </Form.Select>
+              </Form.Group>
+              <Col className="my-auto d-grid gap-2">
+                <Button type="submit" variant="secondary">
+                  Load
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+        )}
 
-        <Table bordered className="mt-5 tableColor">
+        <Table bordered className="mt-5 tableColor" responsive="lg">
           <thead style={{ backgroundColor: '#2867b1' }}>
             <tr className="text-center">
               <th className="textWhite">Student No.</th>
               <th className="textWhite">Name</th>
-              <th className="textWhite">Section Name</th>
+              <th className="textWhite">Grade Level</th>
               <th className="textWhite">Term</th>
+              <th className="textWhite">Section Name</th>
             </tr>
           </thead>
           <tbody>
             {currentUser.faculty.advisory_section_ids.length > 0 ? (
-              sections.map(
-                (section: any) =>
-                  currentUser.faculty.advisory_section_ids.find(
-                    (advisorySection: any) => advisorySection === section._id
-                  ) &&
+              isFiltering ? (
+                filteredSection.length > 0 ? (
+                  filteredSection.map((section: any) =>
+                    section.students.length > 0 ? (
+                      section.students.map((student: any) => (
+                        <tr key={student._id}>
+                          <td>{student.profile.lrn}</td>
+                          <td>
+                            {student.first_name + ' ' + student.last_name}
+                          </td>
+                          <td>{section.grade_level}</td>
+                          <td>{section.term}</td>
+                          <td>{section.section_name}</td>
+                        </tr>
+                      ))
+                    ) : (
+                      <tr className="text-center">
+                        <td colSpan={5}>No Found Student</td>
+                      </tr>
+                    )
+                  )
+                ) : (
+                  <tr className="text-center">
+                    <td colSpan={5}>No Found Section</td>
+                  </tr>
+                )
+              ) : (
+                advisorySection.map((section: any) =>
                   section.students.map((student: any) => (
                     <tr key={student._id}>
                       <td>{student.profile.lrn}</td>
                       <td>{student.first_name + ' ' + student.last_name}</td>
-                      <td>{section.section_name}</td>
+                      <td>{section.grade_level}</td>
                       <td>{section.term}</td>
+                      <td>{section.section_name}</td>
                     </tr>
                   ))
+                )
               )
             ) : (
               <tr className="text-center">
-                <td colSpan={4}>No Advisory Section</td>
+                <td colSpan={5}>No Advisory Section</td>
               </tr>
             )}
           </tbody>
