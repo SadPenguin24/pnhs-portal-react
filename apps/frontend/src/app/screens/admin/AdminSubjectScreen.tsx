83e07cb37714c5a4ebd8fade7752bdbae720c803
cdvsalanga <cdvsalanga@gmail.com>
2023-01-04 23:47:40 +0800

    Filter subjects

--- apps/frontend/src/app/screens/admin/AdminSubjectScreen.tsx
+++ apps/frontend/src/app/screens/admin/AdminSubjectScreen.tsx
@@ -1,5 +1,14 @@
-import React, { useEffect } from 'react';
-import { Button, Container, Spinner, Table } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import {
+  Button,
+  Col,
+  Container,
+  Form,
+  Row,
+  Spinner,
+  Table,
+} from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import Header from '../../components/header/Header';
 import '../../components/tables/tables.scss';
@@ -9,6 +18,11 @@ import { useAppDispatch } from '../../redux/store';
 
 function AdminSubjectScreen() {
   const navigate = useNavigate();
+
+  const { register, handleSubmit } = useForm();
+
+  const [filteredSubject, setFilteredSubject]: any = useState([]);
+  const [isFiltering, setIsFiltering] = useState(false);
 
   const dispatch = useAppDispatch();
 
@@ -24,6 +38,27 @@ function AdminSubjectScreen() {
     dispatch(getSubjects({ subjects }));
   }, [dispatch, subjects]);
 
+  const filterSubject = ({ strand }: any) => {
+    console.log(strand);
+    if (strand === 'All') {
+      console.log('reset');
+      setFilteredSubject([]);
+      setIsFiltering(false);
+    } else {
+      console.log('filter');
+      let filter = subjects;
+      console.log('Filter Subjects');
+      if (strand !== 'All') {
+        filter = filter.filter((subject: any) => subject.strand === strand);
+
+        console.log(filter, filteredSubject);
+      }
+      setFilteredSubject(filter);
+      console.log(filteredSubject);
+      setIsFiltering(true);
+    }
+  };
+
   let content;
 
   if (isLoading) {
@@ -35,52 +70,44 @@ function AdminSubjectScreen() {
       </div>
     );
   } else if (isSuccess) {
+    console.log(subjects);
     content = (
-      <Table bordered className="tableColor">
-        <thead style={{ backgroundColor: '#2a6fd6' }}>
-          <tr className="text-center">
-            <th>Type</th>
-            <th>Strand</th>
-            <th>Subject</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {subjects ? (
-            subjects.map((subject: any) => (
-              <tr key={subject._id}>
-                <td>{subject.type}</td>
-                <td>{subject.strand}</td>
-                <td>{subject.subject_name}</td>
-                <td className="d-flex justify-content-around">
-                  <Button
-                    onClick={() => navigate(`/admin/subject/${subject._id}`)}
-                  >
-                    View
-                  </Button>
-                  {/* <Button variant="danger">Delete</Button> */}
-                </td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan={4} className="text-center">
-                No Subjects
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </Table>
-    );
-  } else if (isError) {
-    content = <p>{JSON.stringify(error)}</p>;
-  }
+      <>
+        <Form onSubmit={handleSubmit(filterSubject)}>
+          <Row>
+            <Col>
+              <Form.Select
+                style={{
+                  backgroundColor: '#ffe4a0',
+                  border: '#eaaa79 solid',
+                }}
+                {...register('strand')}
+              >
+                <option value="All">All Subjects</option>
+                {[
+                  'ABM',
+                  'GAS',
+                  'HUMSS',
+                  'SPORTS',
+                  'STEM',
+                  'TVL-COOKERY',
+                  'TVL-HOME ECONOMICS',
+                  'TVL-ICT',
+                ].map((strand: any) => (
+                  <option value={strand} key={strand}>
+                    {strand}
+                  </option>
+                ))}
+              </Form.Select>
+            </Col>
+            <Col>
+              <Button variant="secondary" type="submit">
+                Load
+              </Button>
+            </Col>
+          </Row>
+        </Form>
 
-  return (
-    <div className="mb-5">
-      <style>{'body { background-color: #dcf7b0; }'}</style>
-      <Header page="Subjects" redirect={`/admin/home`} />
-      <Container>
         <div className="text-end mb-3">
           <Button
             className="me-3"
@@ -90,8 +117,83 @@ function AdminSubjectScreen() {
           </Button>
           {/* <Button variant="danger">Delete All</Button> */}
         </div>
-        {content}
-      </Container>
+
+        <Table bordered className="tableColor">
+          <thead style={{ backgroundColor: '#2a6fd6' }}>
+            <tr className="text-center">
+              <th>Type</th>
+              <th>Strand</th>
+              <th>Subject</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {subjects && subjects.length > 0 ? (
+              isFiltering ? (
+                filteredSubject && filteredSubject.length > 0 ? (
+                  filteredSubject.map((subject: any) => (
+                    <tr key={subject._id}>
+                      <td>{subject.type}</td>
+                      <td>{subject.strand}</td>
+                      <td>{subject.subject_name}</td>
+                      <td className="d-flex justify-content-around">
+                        <Button
+                          onClick={() =>
+                            navigate(`/admin/subject/${subject._id}`)
+                          }
+                        >
+                          View
+                        </Button>
+                        {/* <Button variant="danger">Delete</Button> */}
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td colSpan={4} className="text-center">
+                      No Subjects
+                    </td>
+                  </tr>
+                )
+              ) : (
+                subjects.map((subject: any) => (
+                  <tr key={subject._id}>
+                    <td>{subject.type}</td>
+                    <td>{subject.strand}</td>
+                    <td>{subject.subject_name}</td>
+                    <td className="d-flex justify-content-around">
+                      <Button
+                        onClick={() =>
+                          navigate(`/admin/subject/${subject._id}`)
+                        }
+                      >
+                        View
+                      </Button>
+                      {/* <Button variant="danger">Delete</Button> */}
+                    </td>
+                  </tr>
+                ))
+              )
+            ) : (
+              <tr>
+                <td colSpan={4} className="text-center">
+                  No Subjects
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </Table>
+      </>
+    );
+  } else if (isError) {
+    content = <p>{JSON.stringify(error)}</p>;
+  }
+
+  return (
+    <div className="mb-5">
+      <style>{'body { background-color: #dcf7b0; }'}</style>
+      <Header page="Subjects" redirect={`/admin/home`} />
+      <Container>{content}</Container>
     </div>
   );
 }
